286ec2a483625f5b2829a708714457834bd91ff3
shes50103 <shes50103@gmail.com>
2020-08-29 00:51:55 +0800

    Install select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,9 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+
+import $ from 'jquery'
+import 'select2'
+import 'select2/dist/css/select2.css'
+
+window.$ = $
